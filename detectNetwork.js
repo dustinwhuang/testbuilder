26e781b25e06b6d102645ab0cda325feeedbbb03
Dustin Huang <mail@dustinhuang.com>
2017-09-07 03:53:30 -0700

    Use dictionary, Add Discover and Maestro formats

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,32 +13,84 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-  switch(cardNumber.slice(0,2)+'0000000000000000'.slice(2, cardNumber.length)) {
-    case '38000000000000':
-      return "Diner's Club";
-    case '39000000000000':
-      return "Diner's Club";
-    case '340000000000000':
-      return "American Express";
-    case '370000000000000':
-      return "American Express";
-    case '4100000000000':
-      return "Visa";
-    case '4100000000000000':
-      return "Visa";
-    case '4100000000000000000':
-      return "Visa";
-    case '5100000000000000':
-      return "MasterCard";
-    case '5200000000000000':
-      return "MasterCard";
-    case '5300000000000000':
-      return "MasterCard";
-    case '5400000000000000':
-      return "MasterCard";
-    case '5500000000000000':
-      return "MasterCard";
+  // compare 2 digit prefixes
+  var formats = {
+    //2 digit prefixes
+    38000000000000:"Diner's Club",
+    39000000000000:"Diner's Club",
+    340000000000000:"American Express",
+    370000000000000:"American Express",
+    4100000000000:"Visa",
+    4100000000000000:"Visa",
+    4100000000000000000:"Visa",
+    5100000000000000:"MasterCard",
+    5200000000000000:"MasterCard",
+    5300000000000000:"MasterCard",
+    5400000000000000:"MasterCard",
+    5500000000000000:"MasterCard",
+    6500000000000000:"Discover",
+    6500000000000000000:"Discover",
+
+    // 3 digit prefixes
+    6440000000000000:"Discover",
+    6440000000000000000:"Discover",
+    6450000000000000: "Discover",
+    6450000000000000000:"Discover",
+    6460000000000000:"Discover",
+    6460000000000000000:"Discover",
+    6470000000000000:"Discover",
+    6470000000000000000:"Discover",
+    6480000000000000:"Discover",
+    6480000000000000000:"Discover",
+    6490000000000000:"Discover",
+    6490000000000000000:"Discover",
+
+    // 4 digit prefixes
+    6011000000000000:"Discover",
+    6011000000000000000:"Discover",
+    501800000000:"Maestro",
+    5018000000000:"Maestro",
+    50180000000000:"Maestro",
+    501800000000000:"Maestro",
+    5018000000000000:"Maestro",
+    50180000000000000:"Maestro",
+    501800000000000000:"Maestro",
+    5018000000000000000:"Maestro",
+    502000000000:"Maestro",
+    5020000000000:"Maestro",
+    50200000000000:"Maestro",
+    502000000000000:"Maestro",
+    5020000000000000:"Maestro",
+    50200000000000000:"Maestro",
+    502000000000000000:"Maestro",
+    5020000000000000000:"Maestro",
+    503800000000:"Maestro",
+    5038000000000:"Maestro",
+    50380000000000:"Maestro",
+    503800000000000:"Maestro",
+    5038000000000000:"Maestro",
+    50380000000000000:"Maestro",
+    503800000000000000:"Maestro",
+    5038000000000000000:"Maestro",
+    630400000000:"Maestro",
+    6304000000000:"Maestro",
+    63040000000000:"Maestro",
+    630400000000000:"Maestro",
+    6304000000000000:"Maestro",
+    63040000000000000:"Maestro",
+    630400000000000000:"Maestro",
+    6304000000000000000:"Maestro"
   }
+
+  // Check 2 digit prefixes
+  if(formats[cardNumber.slice(0,2)+'0000000000000000000'.slice(2, cardNumber.length)] !== undefined)
+    return formats[cardNumber.slice(0,2)+'0000000000000000000'.slice(2, cardNumber.length)];
+  // Check 3 digit prefixes
+  if(formats[cardNumber.slice(0,3)+'0000000000000000000'.slice(3, cardNumber.length)] !== undefined)
+    return formats[cardNumber.slice(0,3)+'0000000000000000000'.slice(3, cardNumber.length)];
+  // Check 4 digit prefixes
+  if(formats[cardNumber.slice(0,4)+'0000000000000000000'.slice(4, cardNumber.length)] !== undefined)
+    return formats[cardNumber.slice(0,4)+'0000000000000000000'.slice(4, cardNumber.length)];
 };
 
 
